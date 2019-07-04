bce1187eda6523564dab2c0583443f6fc0e046bb
HarukiKubota <ricca545@icloud.com>
2019-07-04 19:17:32 +0900

    push add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::API
+
+  def created(novel)
+    render json: {
+      attachments: [{
+        color: "#ff6347",
+        pretext: "追加しました。",
+        text: novel.title
+      }]
+    },
+    status: 201
+  end
+
+  def unprocessable_entity(novel)
+    message = {
+      color: "#dc143c",
+      pretext: "登録でエラーが発生しました。",
+      text: novel.errors
+    }
+  end
+
+  def reply_message(text)
+    message = {
+      type: 'text',
+      text: event.message['text']
+    }
+    client.reply_message(event['replyToken'], message)
+  end
+
+  def send_message(user_id, text)
+    message = {
+      type: 'text',
+      text: "メッセージを送信したよ : #{text}"
+    }
+    client.push_message(user_id, message)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,9 @@
 class HomeController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
+  require 'line/bot'
+
+  # GET /
   def index
     render json: {"text": "hello"}
-  end
-
-  # callbackアクションのCSRFトークン認証を無効
-  #protect_from_forgery :except => [:callback]
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
   end
 
   def callback
@@ -29,15 +21,33 @@ class HomeController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          text = event.message['text']
+          user_id = event['source']['userId']
+
+          case text
+          when REGEP_NAROU_URL
+            add_novel(text, user_id)
+          when 'hoge'
+            send_message(text, user_id)
+          end
         end
       end
     }
 
     head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def add_novel
+    novel = Novel.build_by_narou_url(create_params)
+    novel.save ? created(novel) : unprocessable_entity(novel)
   end
 end
