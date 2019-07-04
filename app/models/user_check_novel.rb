d4e4c94efa3a9531e834aed357776cbf58c8fef5
HarukiKubota <ricca545@icloud.com>
2019-07-05 05:32:02 +0900

    fix 一覧

--- app/models/user_check_novel.rb
+++ app/models/user_check_novel.rb
@@ -5,7 +5,7 @@ class UserCheckNovel < ApplicationRecord
   class << self
     def set_entity(user_id, novel_id)
       record = self.where(user_id: user_id, novel_id: novel_id)
-      record ||= self.create(user_id: @user.id, novel_id: novel.id)
+      record ||= self.create(user_id: user_id, novel_id: novel_id)
     end
   end
 end
