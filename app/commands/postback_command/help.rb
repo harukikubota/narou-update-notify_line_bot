269102b47ebc294c6aa6faec8a836c5ddffdf324
HarukiKubota <ricca545@icloud.com>
2019-07-25 13:39:44 +0900

    help 完成

--- app/commands/postback_command/help.rb
+++ app/commands/postback_command/help.rb
@@ -57,4 +57,72 @@ class Help < PostbackCommand
         ②「削除確認メッセージ」が表示されますので、「削除する」を選択してください。
     MES
   end
+
+  def help_writer_list
+    <<~MES.chomp
+      1. 機能説明
+        更新を通知できる作者の一覧を表示します。
+
+      2. 操作方法
+        [トップメニュー]
+          ⇨[一覧メニュー]
+          ⇨[作者一覧]
+    MES
+  end
+
+  def help_writer_add
+    <<~MES.chomp
+      1. 機能説明
+        更新を通知したい作者を追加します。
+
+      2. 操作方法
+        更新を通知したいなろう作者のマイページURLを送信します。
+        例：「https://mypage.syosetu.com/474038/」
+    MES
+  end
+
+  def help_writer_delete
+    <<~MES.chomp
+      1. 機能説明
+        作者の更新通知を解除します。
+
+      2. 操作方法
+        ①更新通知を解除したいなろう作者のマイページURLを送信します。
+          例：「https://mypage.syosetu.com/474038/」
+        ②「削除確認メッセージ」が表示されますので、「削除する」を選択してください。
+    MES
+  end
+
+  def help_separator
+    <<~MES.chomp
+      1. 機能説明
+        通知の合間、メモなどに使用できる区切り線を表示します。
+
+      2. 操作方法
+        [トップメニュー]
+          ⇨[区切り線]
+    MES
+  end
+
+  def help_infomartion
+    <<~MES.chomp
+      1. 機能説明
+        登録上限の確認、各種問い合わせを行えます。
+
+      2. 操作方法
+        [トップメニュー]
+          ⇨[インフォメーション]
+    MES
+  end
+
+  def help_config
+    <<~MES.chomp
+      1. 機能説明
+        各種設定の確認、変更が行えます。
+
+      2. 操作方法
+        [トップメニュー]
+          ⇨[設定]
+    MES
+  end
 end
