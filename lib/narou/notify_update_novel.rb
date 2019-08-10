95ffbd582ac5dad1fc0b8684fef8dbb4302df6a2
HarukiKubota <ricca545@icloud.com>
2019-08-10 18:39:06 +0900

    fix 小説更新通知 フレックスメッセージ使用

--- lib/narou/notify_update_novel.rb
+++ lib/narou/notify_update_novel.rb
@@ -27,38 +27,83 @@ module Narou::NotifyUpdateNovel extend self
 
     # ユーザ一人を対象にメッセージを送信する
     def notify_update_novel_to_user(line_id, notify_datas)
-      notify_novel_count = notify_datas.count
-      message = notify_datas.each_slice(10).each.with_index(1).map do |notify_data_max_10, index|
-        alt_text = "小説更新通知 #{notify_novel_count}件"
-        carousel_ele = CarouselElement.new(alt_text)
-        columns = build_columns(notify_data_max_10)
-        columns.each do |column|
-          carousel_ele.add_column(column)
-        end
-        LineMessage.build_carousel(carousel_ele)
+      messages = notify_datas.each_slice(10).map do |notify_data_max_10|
+        bubbles = build_bubbles(notify_data_max_10)
+        carousel_template(bubbles, notify_datas.count)
       end
-
-      client.push_message(line_id, message)
+      client.push_message(line_id, messages)
     end
 
-    def build_columns(datas)
+    def build_bubbles(datas)
       datas.map do |data|
-        column = CarouselColumn.new(data.title)
         novel_url = "#{Constants::NAROU_NOVEL_URL}/#{data.ncode}/#{data.episode_no}/#{Constants::QUERY_DEFAULT_BROWSER}"
-        column.add_action(action(novel_url))
+        header = header_title(action_do_read(data.title, novel_url))
+        body = body_content(data.episode_no)
+        notify_message_bubble(header, body)
       end
     end
 
-    def action(novel_url)
+    # メッセージのヘッダー
+    def header_title(action)
       {
-        "type": 'uri',
-        "label": '読む',
-        "uri": novel_url
+        type: 'box',
+        layout: 'vertical',
+        contents: [
+          {
+            type: 'button',
+            action: action
+          }
+        ]
+      }
+    end
+
+    def body_content(novel_episode_id)
+      {
+        type: 'box',
+        layout: 'vertical',
+        contents: [
+          {
+            type: 'text',
+            text: "第#{novel_episode_id}話",
+            color: '#e3a368',
+            align: 'center'
+          }
+        ]
+      }
+    end
+
+    # 小説リンクのアクションボタン
+    def action_do_read(novel_title, novel_url)
+      {
+        type: 'uri',
+        label: novel_title,
+        uri: novel_url
       }
     end
 
     def client
       @client ||= LineClient.new.client
+    end
+
+    # 通知メッセージテンプレート １件ごとのバブル
+    def notify_message_bubble(header, body)
+      {
+        type: 'bubble',
+        header: header,
+        body: body
+      }
+    end
+
+    # 最大１０個のバブルを指定できるカルーセルテンプレート
+    def carousel_template(bubbles, notify_count)
+      {
+        type: 'flex',
+        altText: "小説更新通知 #{notify_count}件",
+        contents: {
+          type: 'carousel',
+          contents: bubbles
+        }
+      }
     end
   end
 end
