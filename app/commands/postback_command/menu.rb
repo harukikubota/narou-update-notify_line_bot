46a62d9ee8aa03d1a353473e9c72006b88771752
HarukiKubota <ricca545@icloud.com>
2019-08-28 21:02:30 +0900

    一時保存 base_command->after_call 追加

--- app/commands/postback_command/menu.rb
+++ app/commands/postback_command/menu.rb
@@ -4,14 +4,7 @@ class Menu < PostbackCommand
   end
 
   def call
-    rich_menu_id = RichMenu.find_by_name(@params['name']).rich_menu_id
-    link_menu_to_user(rich_menu_id)
+    @rich_menu_id = RichMenu.find_by_name(@params['name']).rich_menu_id
     @success = true
-  end
-
-  private
-
-  def link_menu_to_user(rich_menu_id)
-    client.link_user_rich_menu(user.line_id, rich_menu_id)
   end
 end
